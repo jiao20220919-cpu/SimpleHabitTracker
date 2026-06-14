44873e5c5909b455f058fb0d89df705ef4c6c585
jiao20220919-cpu <jiao20220919@gmail.com>
2026-06-15 00:52:00 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
-// Top-level build file
 plugins {
-    id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
