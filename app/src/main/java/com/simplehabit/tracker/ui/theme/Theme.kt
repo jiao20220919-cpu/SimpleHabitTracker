b93c0831377931e55510ba1958f43574887a048f
jiao20220919-cpu <jiao20220919@gmail.com>
2026-06-15 23:36:05 +0800

    Update Theme.kt

--- app/src/main/java/com/simplehabit/tracker/ui/theme/Theme.kt
+++ app/src/main/java/com/simplehabit/tracker/ui/theme/Theme.kt
@@ -4,27 +4,17 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-)
+private val DarkColorScheme = darkColorScheme()
+private val LightColorScheme = lightColorScheme()
 
 @Composable
 fun SimpleHabitTrackerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
     MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography(),
+        colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme,
+        typography = MaterialTheme.typography,
         content = content
     )
 }
